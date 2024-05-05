bea339c50431859adca99dfa8be5acc3b8fbfd40
unknown <leonardocolacogg@gmail.com>
2024-05-05 12:52:25 +0300

    frontend started

--- frontend/src/pages/login/Login.jsx
+++ frontend/src/pages/login/Login.jsx
@@ -3,7 +3,9 @@ import React from 'react'
 const Login = () => {
     return (
         <div className="flex flex-col items-center justify-center min-w-96 mx-auto" >
-
+            <div className=''>
+                
+            </div>
             
         </div>
     )
